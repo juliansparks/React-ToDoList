2e2b2bc70525b35700931c58ac9b466eed79a332
Julian Uquillas <juqui001@fiu.edu>
2019-01-31 16:07:42 -0500

    updated for github pages

--- src/App.js
+++ src/App.js
@@ -43,13 +43,13 @@ class App extends Component {
         <div className="App">
           <div className = "container">
             <Header />
-            <Route exact path="/" render={props => (
+            <Route exact path="/React-ToDoList" render={props => (
               <React.Fragment>
                 <AddTodo addTodo = {this.addTodo}/>
                 <Todos todos={this.state.todos} markComplete = {this.markComplete} delTodo={this.delTodo}/>
               </React.Fragment>
             )}/>
-            <Route path="/about" component = {About}/>
+            <Route path="/React-ToDoList/about" component = {About}/>
           </div>
         </div>
       </Router>
