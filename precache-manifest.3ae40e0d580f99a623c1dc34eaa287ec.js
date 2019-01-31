dea86af43aa2cdff5035f0979abca43e69ff0a66
Julian Uquillas <juqui001@fiu.edu>
2019-01-31 16:03:56 -0500

    Updates

---
+++ precache-manifest.3ae40e0d580f99a623c1dc34eaa287ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97b4efd73a9bb69aae6c",
+    "url": "/React-ToDoList/static/js/runtime~main.97b4efd7.js"
+  },
+  {
+    "revision": "1bd67ba90c5a6f699cc0",
+    "url": "/React-ToDoList/static/js/main.1bd67ba9.chunk.js"
+  },
+  {
+    "revision": "c241c8e9abb2bfbc30c2",
+    "url": "/React-ToDoList/static/js/1.c241c8e9.chunk.js"
+  },
+  {
+    "revision": "1bd67ba90c5a6f699cc0",
+    "url": "/React-ToDoList/static/css/main.374b132a.chunk.css"
+  },
+  {
+    "revision": "c01132883fc09dba8859f6e153189f20",
+    "url": "/React-ToDoList/index.html"
+  }
+];
